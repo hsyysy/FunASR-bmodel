8a55debaa4ebb7fe8855adbc6fdc04f095742e97
siyuan.yang <siyuan.yang@sophgo.com>
2024-11-12 11:18:03 +0800

    add online mode with id

--- run_client.sh
+++ run_client.sh
@@ -1,5 +1,3 @@
 cd runtime/python/websocket
-#python3 funasr_wss_client.py --mode 2pass --audio_in ../../../audio/test_audio_12s.wav --port 12333 --chunk_interval 10
-python3 funasr_wss_client.py --mode 2pass --audio_in ../../../audio/test_audio_20241017.wav --port 12333 --chunk_interval 10
-#python3 funasr_wss_client.py --mode online --audio_in ../../../audio/test_audio_20241017.wav --port 12333 --chunk_interval 10
-#python3 funasr_wss_client.py --mode offline --audio_in ../../../audio/test_audio_20241017.wav --port 12333
+#python3 funasr_wss_client.py --mode 2pass --audio_in ../../../audio/test_audio_20241017.wav --port 12333 --chunk_interval 10
+python3 funasr_wss_client_with_id.py --mode 2pass --audio_in ../../../audio/test_audio_20241017.wav --port 12333 --chunk_interval 10
--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 cd runtime/python/websocket
-python3 funasr_wss_server.py --device cpu --dev_id 0 --port 12333
+python3 funasr_wss_server_with_id.py --device cpu --dev_id 0 --port 12333
