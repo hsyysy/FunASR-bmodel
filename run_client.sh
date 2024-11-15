d4fb1e629a4078d492343632a9fd981f28f8a236
siyuan.yang <siyuan.yang@sophgo.com>
2024-11-15 17:42:04 +0800

    fix python scripts

--- run_client.sh
+++ run_client.sh
@@ -1,3 +1,7 @@
 cd runtime/python/websocket
-#python3 funasr_wss_client.py --mode 2pass --audio_in ../../../audio/test_audio_20241017.wav --port 12333 --chunk_interval 10
-python3 funasr_wss_client_with_id.py --mode 2pass --audio_in ../../../audio/test_audio_20241017.wav --port 12333 --chunk_interval 10
+#python3 funasr_wss_client.py \
+python3 funasr_wss_client_with_id.py \
+    --mode 2pass \
+    --audio_in ../../../audio/test_audio_20241017.wav \
+    --port 12333 \
+    --chunk_interval 10
