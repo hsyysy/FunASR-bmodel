9a4747f27e036fd700f2b9e692c4046e73d62036
siyuan.yang <siyuan.yang@sophgo.com>
2024-11-25 18:13:36 +0800

    remove unused script in profile

--- run_client.sh
+++
@@ -1,7 +0,0 @@
-cd runtime/python/websocket
-#python3 funasr_wss_client.py \
-python3 funasr_wss_client_with_id.py \
-    --mode 2pass \
-    --audio_in ../../../audio/test_audio_20241017.wav \
-    --port 12333 \
-    --chunk_interval 10
