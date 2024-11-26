f0ead2f6e9ec2f2c5c7db4869774ff857792c551
siyuan.yang <siyuan.yang@sophgo.com>
2024-11-26 08:52:14 +0800

    fix infer.py

--- infer.py
+++ infer.py
@@ -4,16 +4,16 @@ import multiprocessing
 
 from process_0_info import get_file_dev_id
 
-file_path, dev_id = get_file_dev_id()
+file_path, dev_id, model_dir = get_file_dev_id()
 
 def process():
     # offline asr demo
     model = AutoModel(
-            #model="bmodel/speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-vocab8404",         ## 语音识别模型
-            model="bmodel/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",      ## 语音识别模型
-            vad_model="bmodel/speech_fsmn_vad_zh-cn-16k-common-pytorch",                                  ## 语音端点检测模型
-            punc_model="bmodel/punc_ct-transformer_zh-cn-common-vocab272727-pytorch",                     ## 标点恢复模型
-            spk_model="bmodel/speech_campplus_sv_zh-cn_16k-common",                                       ## 说话人识别模型
+            #model=model_dir+"speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-vocab8404",         ## 语音识别模型
+            model=model_dir+"speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",      ## 语音识别模型
+            vad_model=model_dir+"speech_fsmn_vad_zh-cn-16k-common-pytorch",                                  ## 语音端点检测模型
+            punc_model=model_dir+"punc_ct-transformer_zh-cn-common-vocab272727-pytorch",                     ## 标点恢复模型
+            spk_model=model_dir+"speech_campplus_sv_zh-cn_16k-common",                                       ## 说话人识别模型
             device="cpu",
             disable_update=True,
             disable_pbar=True,
