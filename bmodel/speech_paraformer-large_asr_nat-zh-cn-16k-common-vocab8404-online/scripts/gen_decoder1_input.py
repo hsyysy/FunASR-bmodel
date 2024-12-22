fee1c6a387e0fec6af54d907dfe3436f05ed244d
siyuan.yang <siyuan.yang@sophgo.com>
2024-12-22 11:11:17 +0800

    [bmodel] fix input generator

--- bmodel/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online/scripts/gen_decoder1_input.py
+++ bmodel/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online/scripts/gen_decoder1_input.py
@@ -4,7 +4,7 @@ import torch
 encoder_out = torch.randn(1, 20, 512)
 pre_acoustic_embeds = torch.randn(1, 10, 512)
 pre_token_length = torch.tensor([10], dtype=torch.int64)
-input = {"onnx::MatMul_0":encoder_out.detach().numpy(), "input.1":pre_acoustic_embeds.detach().numpy(), "inp.1":pre_token_length.detach().numpy()}
+input = {"onnx::MatMul_0":encoder_out.detach().numpy(), "input.1":pre_acoustic_embeds.detach().numpy(), "inp":pre_token_length.detach().numpy()}
 for i in range(16):
     input["onnx::Slice_" + str(i+4)] = (torch.randn((1, 512, 50))).detach().numpy()
 np.savez("input_decoder1.npz", **input)
