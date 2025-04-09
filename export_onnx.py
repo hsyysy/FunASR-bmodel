ff511bdb1fdad5103aaf8f2277a1963a303faf91
siyuan.yang <siyuan.yang@sophgo.com>
2025-04-09 15:38:16 +0800

    Add contextual_paraformer export_onnx

---
+++ export_onnx.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+from funasr import AutoModel
+import os
+
+modelname = "iic/speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-vocab8404"
+
+output_dir = "./output"
+os.makedirs(output_dir, exist_ok=True)
+model = AutoModel(model=modelname, device="cuda", disable_update=True, output_dir=output_dir)
+res = model.export(quantize=True, opset_version=13)
--- funasr/models/contextual_paraformer/export_meta.py
+++ funasr/models/contextual_paraformer/export_meta.py
@@ -61,27 +61,45 @@ def export_rebuild_model(model, **kwargs):
     backbone_model = copy.copy(model)
 
     # backbone
-    backbone_model.forward = types.MethodType(export_backbone_forward, backbone_model)
-    backbone_model.export_dummy_inputs = types.MethodType(
-        export_backbone_dummy_inputs, backbone_model
-    )
-    backbone_model.export_input_names = types.MethodType(
-        export_backbone_input_names, backbone_model
-    )
-    backbone_model.export_output_names = types.MethodType(
-        export_backbone_output_names, backbone_model
-    )
-    backbone_model.export_dynamic_axes = types.MethodType(
-        export_backbone_dynamic_axes, backbone_model
-    )
+    part = "encoder"
+    if part == "encoder":
+        backbone_model.forward = types.MethodType(export_backbone_forward_encoder, backbone_model)
+        backbone_model.export_dummy_inputs = types.MethodType(
+            export_backbone_dummy_inputs_encoder, backbone_model
+        )
+        backbone_model.export_input_names = types.MethodType(
+            export_backbone_input_names_encoder, backbone_model
+        )
+        backbone_model.export_output_names = types.MethodType(
+            export_backbone_output_names_encoder, backbone_model
+        )
+        backbone_model.export_dynamic_axes = types.MethodType(
+            export_backbone_dynamic_axes_encoder, backbone_model
+        )
+        backbone_model.export_name = types.MethodType(export_backbone_name_encoder, backbone_model)
+    elif part == "decoder":
+        backbone_model.forward = types.MethodType(export_backbone_forward_decoder, backbone_model)
+        backbone_model.export_dummy_inputs = types.MethodType(
+            export_backbone_dummy_inputs_decoder, backbone_model
+        )
+        backbone_model.export_input_names = types.MethodType(
+            export_backbone_input_names_decoder, backbone_model
+        )
+        backbone_model.export_output_names = types.MethodType(
+            export_backbone_output_names_decoder, backbone_model
+        )
+        backbone_model.export_dynamic_axes = types.MethodType(
+            export_backbone_dynamic_axes_decoder, backbone_model
+        )
+        backbone_model.export_name = types.MethodType(export_backbone_name_decoder, backbone_model)
     
     embedder_model.export_name = "model_eb"
-    backbone_model.export_name = "model"
+    #backbone_model.export_name = "model"
 
     return backbone_model, embedder_model
 
 
-def export_backbone_forward(
+def export_backbone_forward_ori(
     self,
     speech: torch.Tensor,
     speech_lengths: torch.Tensor,
@@ -100,22 +118,80 @@ def export_backbone_forward(
     return decoder_out, pre_token_length
 
 
-def export_backbone_dummy_inputs(self):
+def export_backbone_forward_encoder(
+            self,
+            speech: torch.Tensor,
+            speech_lengths: torch.Tensor,
+    ):
+    batch = {"speech": speech, "speech_lengths": speech_lengths}
+    
+    enc, enc_len = self.encoder(**batch)
+    mask = self.make_pad_mask(enc_len)[:, None, :]
+    hidden, alphas, token_num = self.predictor(enc, mask)
+    return enc, hidden, alphas, token_num
+
+
+def export_backbone_forward_decoder(
+            self,
+            enc: torch.Tensor,
+            enc_len: torch.Tensor,
+            pre_acoustic_embeds: torch.Tensor,
+            pre_token_length: torch.Tensor,
+            bias_embed: torch.Tensor,
+    ):
+    decoder_out, _ = self.decoder(enc, enc_len, pre_acoustic_embeds, pre_token_length, bias_embed)
+    decoder_out = torch.log_softmax(decoder_out, dim=-1)
+
+    return decoder_out
+
+
+def export_backbone_dummy_inputs_ori(self):
     speech = torch.randn(2, 30, self.feats_dim)
     speech_lengths = torch.tensor([6, 30], dtype=torch.int32)
     bias_embed = torch.randn(2, 1, 512)
     return (speech, speech_lengths, bias_embed)
 
 
-def export_backbone_input_names(self):
+def export_backbone_dummy_inputs_encoder(self):
+    speech = torch.randn(2, 30, self.feats_dim)
+    speech_lengths = torch.tensor([6, 30], dtype=torch.int32)
+    return (speech, speech_lengths) #export encoder
+
+
+def export_backbone_dummy_inputs_decoder(self):
+    enc = torch.randn(2, 30, 512)
+    enc_len = torch.tensor([6, 30], dtype=torch.int32)
+    pre_acoustic_embeds = torch.randn(2, 9, 512)
+    pre_token_length = torch.tensor([6, 9], dtype=torch.int32)
+    bias_embed = torch.randn(2, 1, 512)
+    return (enc, enc_len, pre_acoustic_embeds, pre_token_length, bias_embed) #export decoder
+
+
+def export_backbone_input_names_ori(self):
     return ["speech", "speech_lengths", "bias_embed"]
 
 
-def export_backbone_output_names(self):
+def export_backbone_input_names_encoder(self):
+    return ['speech', 'speech_lengths'] #export encoder
+
+
+def export_backbone_input_names_decoder(self):
+    return ['enc', 'enc_len', 'pre_acoustic_embeds', 'pre_token_length', 'bias_embed'] #export decoder
+
+
+def export_backbone_output_names_ori(self):
     return ["logits", "token_num"]
 
 
-def export_backbone_dynamic_axes(self):
+def export_backbone_output_names_encoder(self):
+    return ['enc', 'hidden', 'alphas', 'token_num'] #export encoder
+
+
+def export_backbone_output_names_decoder(self):
+    return ['logits'] #export decoder
+
+
+def export_backbone_dynamic_axes_ori(self):
     return {
         "speech": {0: "batch_size", 1: "feats_length"},
         "speech_lengths": {
@@ -126,5 +202,68 @@ def export_backbone_dynamic_axes(self):
     }
 
 
-def export_backbone_name(self):
+def export_backbone_dynamic_axes_encoder(self):
+    #encoder
+    return {
+        'speech': {
+            0: 'batch_size',
+            1: 'feats_length'
+        },
+        'speech_lengths': {
+            0: 'batch_size',
+        },
+        'enc': {
+            0: 'batch_size',
+            1: 'feats_length'
+        },
+        'hidden': {
+            0: 'batch_size',
+            1: 'logits_length_plus'
+        },
+        'alphas': {
+            0: 'batch_size',
+            1: 'logits_length_plus'
+        },
+        # 'token_num': {
+        #     0: 'batch_size',
+        # },
+    }
+
+
+def export_backbone_dynamic_axes_decoder(self):
+    #decoder
+    return {
+        'enc': {
+            0: 'batch_size',
+            1: 'feats_length'
+        },
+        'enc_len': {
+            0: 'batch_size',
+        },
+        'pre_acoustic_embeds': {
+            0: 'batch_size',
+            1: 'unknown_length'
+        },
+        'pre_token_length': {
+            0: 'batch_size',
+        },
+        'bias_embed': {
+            0: 'batch_size',
+            1: 'num_hotwords'
+        },
+        'logits': {
+            0: 'batch_size',
+            1: 'logits_length'
+        },
+    }
+
+def export_backbone_name_ori(self):
     return "model.onnx"
+
+
+def export_backbone_name_encoder(self):
+    return 'encoder.onnx'
+
+
+def export_backbone_name_decoder(self):
+    return 'decoder.onnx'
