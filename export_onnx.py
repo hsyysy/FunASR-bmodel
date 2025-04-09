ea06b2dbf09da9907dff80c70d844da1d5522afc
siyuan.yang <siyuan.yang@sophgo.com>
2025-04-09 16:49:40 +0800

    fix scripts

--- export_onnx.py
+++ export_onnx.py
@@ -7,4 +7,4 @@ modelname = "iic/speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-voc
 output_dir = "./output"
 os.makedirs(output_dir, exist_ok=True)
 model = AutoModel(model=modelname, device="cpu", disable_update=True, output_dir=output_dir)
-res = model.export(quantize=True, opset_version=13)
+res = model.export(quantize=False, opset_version=13)
